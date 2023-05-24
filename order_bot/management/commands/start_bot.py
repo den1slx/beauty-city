8339634aed1454c374aa6c6db5ec6a711fad176d
den1slx <keycoingold@mail.ru>
2023-05-24 20:24:49 +1000

    Add strart_bot to manage

--- order_bot/management/commands/start_bot.py
+++ order_bot/management/commands/start_bot.py
@@ -1,1 +1,9 @@
-# тут код для запуска бота
+from django.core.management.base import BaseCommand
+from order_bot.bot_handlers import run_bot
+
+
+class Command(BaseCommand):
+    help = 'run bot'
+
+    def handle(self, *args, **options):
+        run_bot()
