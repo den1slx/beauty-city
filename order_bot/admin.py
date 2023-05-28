d7e91a20b089568d9e7ab00f896778bd7cc5c680
den1slx <keycoingold@mail.ru>
2023-05-28 23:38:40 +1000

    Update admin.py

--- order_bot/admin.py
+++ order_bot/admin.py
@@ -1,43 +1,47 @@
 from django.contrib import admin
 
-from order_bot.models import Client, Master, Procedure, Salon, Appointment, WorkTime, WorkDay
-
-
-@admin.register(Client)
-class ClientAdmin(admin.ModelAdmin):
-    fields = ('name', 'phone_number')
-    list_display = ('name', 'phone_number')
+from order_bot.models import Master, Procedure, Salon, Appointment, WorkTime, WorkDay, Promo
 
 
 @admin.register(Master)
 class MasterAdmin(admin.ModelAdmin):
-    fields = ('name', 'service', 'phone_number', 'worktime')
+    fields = ('name', 'service', 'phone_number')
     list_display = ('name', 'phone_number')
+    list_filter = ('service',)
 
 
 @admin.register(Procedure)
 class ProcedureAdmin(admin.ModelAdmin):
     fields = ('title', 'price')
+    ordering = ('id',)
 
 
 @admin.register(Salon)
 class SalonAdmin(admin.ModelAdmin):
-    fields = ('title', 'address', 'open_time', 'close_time', 'procedures', 'workers', 'days')
+    fields = ('title', 'address', 'open_time', 'close_time', 'procedures', 'workers')
     list_display = ('title', 'address')
 
 
 @admin.register(Appointment)
 class AppointmentAdmin(admin.ModelAdmin):
-    fields = ('date', 'salon', 'client', 'master', 'payment')
-    list_display = ('date', 'salon', 'client', 'master', 'payment')
+    fields = ('date', 'salon', 'name', 'phone_number', 'master', 'service', 'payment')
+    list_display = ('date', 'salon', 'master', 'payment')
     readonly_fields = ('payment',)
 
 
 @admin.register(WorkTime)
 class WorkTimeAdmin(admin.ModelAdmin):
-    fields = ('begin', 'end', 'worker')
+    fields = ('begin', 'end', 'worker', 'workplace')
+    list_display = ('workplace', 'worker', 'begin', 'end')
+    list_filter = ('workplace', 'worker')
 
 
 @admin.register(WorkDay)
 class WorkDayAdmin(admin.ModelAdmin):
     fields = ('day', 'worktime')
+
+
+@admin.register(Promo)
+class PromoAdmin(admin.ModelAdmin):
+    fields = ('promo', 'status')
+    list_filter = ('status',)
